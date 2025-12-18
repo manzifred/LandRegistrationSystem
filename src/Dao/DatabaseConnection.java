c4f74135e3c40272c9c596c63f048646ac8192e7
manzifred <fredmanzi005@gmail.com>
2025-12-18 14:59:28 +0200

    dockerize

--- src/Dao/DatabaseConnection.java
+++ src/Dao/DatabaseConnection.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  */
 public class DatabaseConnection {
     
-    private static final String URL = "jdbc:postgresql://localhost:5432/land_registration_db";
+    private static final String URL = "jdbc:postgresql://host.docker.internal:5432/land_registration_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     
